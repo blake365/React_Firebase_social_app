ee4c6ac5cd502176b2239874e94dd86b045996b7
blake365 <68243611+blake365@users.noreply.github.com>
2020-10-12 20:16:42 -0400

    added express and converted routes

--- functions/index.js
+++ functions/index.js
@@ -1,50 +1,50 @@
-const functions = require('firebase-functions');
+const functions = require('firebase-functions')
 const admin = require('firebase-admin')
 
 admin.initializeApp()
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-exports.helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase!");
-});
 
-exports.getScreams = functions.https.onRequest((request, response) => {
-    admin.firestore().collection('screams').get().then(data =>
-    {
-        let screams = []
-        data.forEach(doc =>
-        {
-            screams.push(doc.data())
+const express = require('express')
+const app = express()
+
+app.get('/screams', (req, res) => {
+  admin
+    .firestore()
+    .collection('screams')
+    .orderBy('createdAt', 'desc')
+    .get()
+    .then(data => {
+      let screams = []
+      data.forEach(doc => {
+        screams.push({
+          screamId: doc.id,
+          body: doc.data().body,
+          userHandle: doc.data().userHandle,
+          createdAt: doc.data().createdAt,
         })
-        return response.json(screams)
+      })
+      return res.json(screams)
     })
     .catch(err => console.error(err))
 })
 
-exports.createScream = functions.https.onRequest((request, response) =>
-{
-    if (request.method !== 'POST') {
-        return response.status(400).json({
-            error: 'Method Not Allowed'
-        })
-    }
-    const newScream = {
-        body: request.body.body,
-        userHandle: request.body.userHandle,
-        createdAt: admin.firestore.Timestamp.fromDate(new Date()),
-    };
-    
-    admin.firestore().collection('screams').add(newScream)
-        .then(doc =>
-        {
-            response.json({ message: `document ${doc.id} created successfully`})
-        })
-        .catch(err =>
-        {
-            response.status(500).json({ error: 'something went wrong' })
-            console.error(err)
-        })
-}) 
+app.post('/scream', (req, res) => {
+  const newScream = {
+    body: req.body.body,
+    userHandle: req.body.userHandle,
+    createdAt: new Date().toISOString(),
+  }
 
+  admin
+    .firestore()
+    .collection('screams')
+    .add(newScream)
+    .then(doc => {
+      res.json({ message: `document ${doc.id} created successfully` })
+    })
+    .catch(err => {
+      res.status(500).json({ error: 'something went wrong' })
+      console.error(err)
+    })
+})
+
+exports.api = functions.https.onRequest(app)
