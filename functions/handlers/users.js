3cdbc49b9a1762afe9581fc15af2407452989426
blake365 <68243611+blake365@users.noreply.github.com>
2020-10-17 17:48:15 -0400

    added comment route to add comments to screams

--- functions/handlers/users.js
+++ functions/handlers/users.js
@@ -4,8 +4,13 @@ const firebase = require('firebase')
 firebase.initializeApp(config)
 const { uuid } = require('uuidv4')
 
-const { validateSignupData, validateLoginData } = require('../util/validators')
+const {
+  validateSignupData,
+  validateLoginData,
+  reduceUserDetails,
+} = require('../util/validators')
 
+// signup
 exports.signup = (req, res) => {
   const newUser = {
     email: req.body.email,
@@ -92,6 +97,48 @@ exports.login = (req, res) => {
     })
 }
 
+// get own user details
+exports.getAuthenticatedUser = (req, res) => {
+  let userData = {}
+  db.doc(`/users/${req.user.handle}`)
+    .get()
+    .then(doc => {
+      if (doc.exists) {
+        userData.credentials = doc.data()
+        return db
+          .collection('likes')
+          .where('userHandle', '==', req.user.handle)
+          .get()
+      }
+    })
+    .then(data => {
+      userData.likes = []
+      data.forEach(doc => {
+        userData.likes.push(doc.data())
+      })
+      return res.json(userData)
+    })
+    .catch(err => {
+      console.error(err)
+      return res.status(500).json({ error: err.code })
+    })
+}
+
+// Add user details
+exports.addUserDetails = (req, res) => {
+  let userDetails = reduceUserDetails(req.body)
+
+  db.doc(`/users/${req.user.handle}`)
+    .update(userDetails)
+    .then(() => {
+      return res.json({ message: 'Details added successfully' })
+    })
+    .catch(err => {
+      console.error(err)
+      return res.status(500).json({ error: err.code })
+    })
+}
+
 //profile image upload
 exports.uploadImage = (req, res) => {
   const BusBoy = require('busboy')
@@ -149,3 +196,5 @@ exports.uploadImage = (req, res) => {
   })
   busboy.end(req.rawBody)
 }
+
+//
--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,19 @@
 const functions = require('firebase-functions')
-const { getAllScreams, postOneScream } = require('./handlers/screams')
-const { signup, login, uploadImage } = require('./handlers/users')
+const {
+  getAllScreams,
+  postOneScream,
+  getScream,
+  commentOnScream,
+} = require('./handlers/screams')
+
+const {
+  signup,
+  login,
+  uploadImage,
+  addUserDetails,
+  getAuthenticatedUser,
+} = require('./handlers/users')
+
 const express = require('express')
 const app = express()
 const FBAuth = require('./util/FBAuth')
@@ -9,12 +22,17 @@ app.use(express.urlencoded({ extended: true }))
 
 // scream routes
 app.get('/screams', getAllScreams)
-//post a scream
 app.post('/scream', FBAuth, postOneScream)
-
+app.get('/scream/:screamId', getScream)
+// TODO: delete scream
+// TODO: like a scream
+// TODO: unlike a scream
+app.post('/scream/:screamId/comment', FBAuth, commentOnScream)
 // User routes
 app.post('/signup', signup)
 app.post('/login', login)
 app.post('/user/image', FBAuth, uploadImage)
+app.post('/user/', FBAuth, addUserDetails)
+app.get('/user', FBAuth, getAuthenticatedUser)
 
 exports.api = functions.https.onRequest(app)
