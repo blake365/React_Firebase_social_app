80ad62a3e3c309db3306ae7a8997da7bbcd9264c
blake365 <68243611+blake365@users.noreply.github.com>
2020-10-17 10:45:50 -0400

    refactored code into multiple files

---
+++ functions/util/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  apiKey: 'AIzaSyBI-BbwbpTWLR0tYyeKSC5tRn1pGd7xtjg',
+  authDomain: 'socialapes-20bf7.firebaseapp.com',
+  databaseURL: 'https://socialapes-20bf7.firebaseio.com',
+  projectId: 'socialapes-20bf7',
+  storageBucket: 'socialapes-20bf7.appspot.com',
+  messagingSenderId: '995218645124',
+  appId: '1:995218645124:web:e6675a081627f39430a2d1',
+  measurementId: 'G-R2EZMPQQ65',
+}
