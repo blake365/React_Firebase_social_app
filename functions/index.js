83ddb52fca473b24904ac5c9e3f3e2e316fc662d
blake365 <68243611+blake365@users.noreply.github.com>
2020-10-18 12:40:35 -0400

    added like and unlike feature for screams

--- functions/index.js
+++ functions/index.js
@@ -4,6 +4,9 @@ const {
   postOneScream,
   getScream,
   commentOnScream,
+  likeScream,
+  unlikeScream,
+  deleteScream,
 } = require('./handlers/screams')
 
 const {
@@ -24,15 +27,17 @@ app.use(express.urlencoded({ extended: true }))
 app.get('/screams', getAllScreams)
 app.post('/scream', FBAuth, postOneScream)
 app.get('/scream/:screamId', getScream)
-// TODO: delete scream
-// TODO: like a scream
+app.delete('/scream/:screamId', FBAuth, deleteScream)
+app.get('/scream/:screamId/like', FBAuth, likeScream)
+app.get('/scream/:screamId/unlike', FBAuth, unlikeScream)
 // TODO: unlike a scream
 app.post('/scream/:screamId/comment', FBAuth, commentOnScream)
+
 // User routes
 app.post('/signup', signup)
 app.post('/login', login)
 app.post('/user/image', FBAuth, uploadImage)
-app.post('/user/', FBAuth, addUserDetails)
+app.post('/user', FBAuth, addUserDetails)
 app.get('/user', FBAuth, getAuthenticatedUser)
 
 exports.api = functions.https.onRequest(app)
